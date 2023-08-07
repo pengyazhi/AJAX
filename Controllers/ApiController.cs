15e3e828845be2155bc76cc4789eece232178dbd
pengyazhi <yzyz86031614@gmail.com>
2023-08-07 21:04:24 +0800

    ajax作業

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AJAX.Controllers
+{
+    public class ApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello Ajax!!");
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,5 +28,13 @@ namespace AJAX.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult First()
+        {
+            return View();
+        }
+        public IActionResult Travel()
+        {
+            return View();
+        }
     }
 }
